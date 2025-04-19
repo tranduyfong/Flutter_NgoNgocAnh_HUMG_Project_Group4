9fec5ce992bad43b301ef69ad0acb0ee84701af2
tranduyfong <tranphong86958@gmail.com>
2025-04-19 10:42:52 +0700

    Chinh sua import va add Widget Moi

---
+++ lib/widgets/new.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class NewWidget extends StatefulWidget {
+  const NewWidget({super.key});
+
+  @override
+  State<NewWidget> createState() => _NewWidget();
+}
+
+class _NewWidget extends State<NewWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: 100,
+        shrinkWrap: true,
+        physics: NeverScrollableScrollPhysics(),
+        itemBuilder: (context, index) {
+          return Column(
+            children: [
+              SizedBox(
+                height: 100,
+                child: Center(
+                  child: Text(
+                    'Title $index',
+                    style: TextTheme.of(context).headlineSmall,
+                  ),
+                ),
+              ),
+              Divider(
+                height: 5,
+              )
+            ],
+          );
+        });
+  }
+}
