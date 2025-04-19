adcb7469cf16de19256dd2d5bdfcff6c62c298c9
tranduyfong <tranphong86958@gmail.com>
2025-04-19 10:31:06 +0700

    Phan chia cac tab va navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:namer_app/screens/myaccount.dart';
+import 'package:namer_app/screens/trending.dart';
+import 'screens/news.dart';
 
-void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: SafeArea(child: Scaffold(body: MyApp(),)),
-  ));
+void main(List<String> args) {
+  runApp(
+    MaterialApp(
+      home: BottomNavigationProject(),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class BottomNavigationProject extends StatefulWidget {
+  const BottomNavigationProject({super.key});
+  @override
+  State<BottomNavigationProject> createState() => _BottomNavigationProject();
+}
+
+class _BottomNavigationProject extends State<BottomNavigationProject> {
+  int _selectedIndex = 0;
+  static const List<Widget> _widgetOptions = <Widget>[
+    NewsScreen(),
+    TrendingScreen(),
+    MyAccountScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Hello world", style: TextStyle(color: Colors.pink, fontWeight: FontWeight.bold)),);
+    return Scaffold(
+      body: _widgetOptions.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.list_alt), label: 'Tin tức'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.trending_up),
+            label: 'Xu hướng',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Cá nhân'),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue,
+        onTap: _onItemTapped,
+      ),
+    );
   }
-
 }
-
