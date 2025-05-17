7437812d41a50e51ea0d531a2e050039c034e0f5
tranduyfong <tranphong86958@gmail.com>
2025-05-17 09:04:43 +0700

    Hoan thanh mot so chuc nang

--- lib/models/api.dart
+++ lib/models/api.dart
@@ -4,8 +4,9 @@ import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
 class DataService {
-  static final String baseUrl = 'http://192.168.110.68:3000';
+  static final String baseUrl = 'http://192.168.1.18:3000';
 
+  // API to get all data of new
   Future<List<dynamic>> getAllData() async {
     final response = await http.get(Uri.parse('$baseUrl/'));
     if (response.statusCode == 200) {
@@ -15,6 +16,7 @@ class DataService {
     }
   }
 
+  // API to get data some article
   Future<List<dynamic>> getDataSomeNews(int idBao) async {
     final response = await http.get(Uri.parse('$baseUrl/getNews/$idBao'));
     if (response.statusCode == 200) {
@@ -24,6 +26,7 @@ class DataService {
     }
   }
 
+  // API to remove article in database
   static Future<Map<String, dynamic>> deleteArticleFavourite(int idBao) async {
     final prefs = await SharedPreferences.getInstance();
     final token = prefs.getString('jwtToken');
@@ -42,9 +45,13 @@ class DataService {
     }
   }
 
+  // API to add new article in database
   static Future<Map<String, dynamic>> addArticleFavourite(int idBao) async {
     final prefs = await SharedPreferences.getInstance();
     final token = prefs.getString('jwtToken');
+    if (token == null || token.isEmpty) {
+      throw Exception('can not find token');
+    }
 
     final response = await http.post(
       Uri.parse('$baseUrl/favourite/$idBao/like'),
@@ -56,10 +63,11 @@ class DataService {
     if (response.statusCode == 200) {
       return jsonDecode(response.body);
     } else {
-      throw Exception('Không thể tải dữ liệu');
+      throw Exception('Error...');
     }
   }
 
+  // API to get token JWT
   static Future<String?> getTokenLogin(String email, String password) async {
     final response = await http.post(
       Uri.parse('$baseUrl/login'),
@@ -75,6 +83,7 @@ class DataService {
     }
   }
 
+  // API to check the user is liked the article
   Future<List<dynamic>> checkLikedArticle(int idBao) async {
     final response = await http.get(
       Uri.parse('$baseUrl/checkLikedArticle/$idBao'),
@@ -83,6 +92,94 @@ class DataService {
       return jsonDecode(response.body);
     } else {
       throw Exception('Không thể tải dữ liệu');
+    }
+  }
+
+  // The function to check liked article
+  Future<bool> checkLiked(int idBao) async {
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString('jwtToken');
+
+    if (token == null) {
+      return false;
+    }
+
+    final response = await http.get(
+      Uri.parse('$baseUrl/checkLikedArticle/$idBao'),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $token',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      if (data is List && data.isNotEmpty) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
+
+  // The function to get data user
+  Future<List<dynamic>> getUserData() async {
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString('jwtToken');
+
+    // Check the token is invalid
+    if (token == null || token.isEmpty) {
+      throw Exception('Token khong ton tai');
+    }
+
+    final response = await http.get(
+      Uri.parse('$baseUrl/user/data'),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $token',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception('Loi van de');
+    }
+  }
+
+  Future<Map<String, dynamic>> addNewUser(
+    String name,
+    String email,
+    String password,
+  ) async {
+    final response = await http.post(
+      Uri.parse('$baseUrl/create/user'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({'name': name, 'email': email, 'password': password}),
+    );
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception('Loi van de');
+    }
+  }
+
+  Future<bool> checkExistAccount(String email) async {
+    final response = await http.get(
+      Uri.parse('$baseUrl/checkExistAccount/$email'),
+    );
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      if (data is List && data.isNotEmpty) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return false;
     }
   }
 }
--- lib/screens/trending.dart
+++ lib/screens/trending.dart
@@ -1,22 +1,157 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
-class TrendingScreen extends StatelessWidget {
+class TrendingScreen extends StatefulWidget {
   const TrendingScreen({super.key});
 
   @override
+  State<TrendingScreen> createState() => _TrendingScreen();
+}
+
+class _TrendingScreen extends State<TrendingScreen> {
+  late VideoPlayerController _videoPlayerController;
+  PageController pageController = PageController();
+  late Uri pathUrl;
+
+  Widget _buildProgressBar() {
+    final position = _videoPlayerController.value.position;
+    final duration = _videoPlayerController.value.duration;
+
+    final progress =
+        duration.inMilliseconds > 0
+            ? position.inMilliseconds / duration.inMilliseconds
+            : 0.0;
+
+    return LinearProgressIndicator(
+      value: progress,
+      valueColor: AlwaysStoppedAnimation<Color>(Colors.grey),
+      backgroundColor: const Color.fromARGB(255, 75, 73, 73),
+      minHeight: 1,
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _videoPlayerController =
+        VideoPlayerController.networkUrl(
+            Uri.parse(
+              'https://streaming-cms-tpo.epicdn.me/aae964e2df4fd050df7ea4f2cdbf35b1/681845d0/2025_05_04/c0049_4760.mp4',
+            ),
+          )
+          ..addListener(() {
+            if (mounted) {
+              setState(() {});
+            }
+          })
+          ..setLooping(true)
+          ..initialize().then((_) {
+            setState(() {}); // Khi video đã sẵn sàng
+            _videoPlayerController.play(); // Tự động phát
+          });
+  }
+
+  @override
+  void dispose() {
+    _videoPlayerController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        AppBar(
-          toolbarHeight: 50,
-          backgroundColor: Colors.blue,
-          title: Center(
-            child: Text('Xu hướng', style: TextStyle(color: Colors.white)),
+    return PageView.builder(
+      itemCount: 3,
+      scrollDirection: Axis.vertical,
+      controller: pageController,
+      onPageChanged: (value) {
+        setState(() {
+          _videoPlayerController.play();
+          _videoPlayerController.seekTo(Duration.zero);
+        });
+      },
+      itemBuilder: (context, index) {
+        return Container(
+          color: Colors.black,
+          child: Column(
+            children: [
+              Expanded(
+                flex: 6,
+                child:
+                    _videoPlayerController.value.isInitialized
+                        ? GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              _videoPlayerController.value.isPlaying
+                                  ? _videoPlayerController.pause()
+                                  : _videoPlayerController.play();
+                            });
+                          },
+                          child: FittedBox(
+                            fit: BoxFit.contain,
+                            child: SizedBox(
+                              width: _videoPlayerController.value.size.width,
+                              height: _videoPlayerController.value.size.height,
+                              child: AspectRatio(
+                                aspectRatio:
+                                    _videoPlayerController.value.aspectRatio,
+                                child: VideoPlayer(_videoPlayerController),
+                              ),
+                            ),
+                          ),
+                        )
+                        : Container(),
+              ),
+              Expanded(
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Row(
+                              children: [
+                                ClipRRect(
+                                  borderRadius: BorderRadius.circular(15),
+                                  child: SizedBox(
+                                    height: 30,
+                                    width: 30,
+                                    child: Image.network(
+                                      'https://yt3.googleusercontent.com/wVJk5uLYiKqPT5_Ucz5gxGJxMVdIHG1TDz5HXrlRR_JSRpMyhxOvQJzqy91XidLXH0Z6cfZyfMQ=s900-c-k-c0x00ffffff-no-rj',
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(width: 10),
+                                Text(
+                                  'Tien Phong',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Text(
+                              'Người dân trở lại Thủ đô sau lễ, cửa ngõ phía Nam ùn ứ, nội đô thông thoáng',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 15,
+                              ),
+                              maxLines: 2,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    _buildProgressBar(),
+                  ],
+                ),
+              ),
+            ],
           ),
-        ),
-        Center(child: Text('Title of Trending')),
-      ],
+        );
+      },
     );
   }
 }
